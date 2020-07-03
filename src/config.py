24bf9fb74e5b04772533772a1b757758ebebc147
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-07-03 12:32:38 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ class Development(object):
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DEBUG = True
     TESTING = False
+    THREADED = True
     JWT_SECRET_KEY = 'gefgiuwhui7969874u9ihrio7y348yhiuwehoiuf0(*&G*)&'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'tcs.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -15,6 +16,7 @@ class Development(object):
 class Production(object):
     DEBUG = False
     TESTING = False
+    THREADED = True
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
